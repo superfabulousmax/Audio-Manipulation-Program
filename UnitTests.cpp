b419158656a78a756d4674c41d53cf934477899f
Sinead Urisohn <sinzara611@gmail.com>
2016-05-08 12:23:42 +0200

    Implemented unit test case for mono audio cut.

--- UnitTests.cpp
+++ UnitTests.cpp
@@ -75,6 +75,40 @@ TEST_CASE("Mono audio concatenation","This tests that concatenation of two mono 
 
 }
 
+TEST_CASE("Stereo audio concatenation","This tests that concatenation of two stereo audio files works correctly.")
+{
+
+	std::vector<std::pair<int16_t, int16_t >> buffer1 = {std::make_pair(int8_t(1), int8_t(2))};
+	std::vector<std::pair<int16_t, int16_t >> buffer2 = {std::make_pair(int8_t(3), int8_t(4))};
+	std::vector<std::pair<int16_t, int16_t >> catBuffer = {std::make_pair(int8_t(1), int8_t(2)), std::make_pair(int8_t(3), int8_t(4))};
+	AudioArray <std::pair<int16_t, int16_t >, 2> array1(buffer1);
+	AudioArray <std::pair<int16_t, int16_t >, 2> array2(buffer2);
+	AudioArray <std::pair<int16_t, int16_t >, 2> cat = array1 | array2;
+
+	for(int i = 0; i < cat.getSize(); ++i) {
+		REQUIRE(cat[i].first == catBuffer[i].first);
+		REQUIRE(cat[i].second == catBuffer[i].second);
+	}
+
+}
+
+TEST_CASE("Mono audio cut","This tests that the cutting of over the given range for mono audio files works correctly.")
+{
+	std::vector<int8_t> buffer = {int8_t(1), int8_t(2), int8_t(3), int8_t(4)};
+	std::vector<int8_t> cutBuffer = {int8_t(1), int8_t(4)};
+	AudioArray <int8_t> array(buffer);
+	AudioArray <int8_t> cut = array ^ std::make_pair(1, 2);
+
+	for(int i = 0; i < cut.getSize(); ++i) {
+		REQUIRE(cut[i] == cutBuffer[i]);
+	}
+}
+
+// TEST_CASE("Stereo audio cut","This tests that the cutting of over the given range for stereo audio files works correctly.")
+// {
+
+// }
+
 TEST_CASE("Mono audio reverse","This tests that reverse of mono audio files works correctly.")
 {
 	std::vector<int8_t> buffer = {int8_t(1), int8_t(2)};
@@ -96,22 +130,7 @@ TEST_CASE("Mono audio rms","This tests that the calculation of RMS for mono audi
 	REQUIRE(array.computeRMS() == result);
 }
 
-TEST_CASE("Stereo audio concatenation","This tests that concatenation of two stereo audio files works correctly.")
-{
 
-	std::vector<std::pair<int16_t, int16_t >> buffer1 = {std::make_pair(int8_t(1), int8_t(2))};
-	std::vector<std::pair<int16_t, int16_t >> buffer2 = {std::make_pair(int8_t(3), int8_t(4))};
-	std::vector<std::pair<int16_t, int16_t >> catBuffer = {std::make_pair(int8_t(1), int8_t(2)), std::make_pair(int8_t(3), int8_t(4))};
-	AudioArray <std::pair<int16_t, int16_t >, 2> array1(buffer1);
-	AudioArray <std::pair<int16_t, int16_t >, 2> array2(buffer2);
-	AudioArray <std::pair<int16_t, int16_t >, 2> cat = array1 | array2;
-
-	for(int i = 0; i < cat.getSize(); ++i) {
-		REQUIRE(cat[i].first == catBuffer[i].first);
-		REQUIRE(cat[i].second == catBuffer[i].second);
-	}
-
-}
 
 TEST_CASE("Mono audio ranged add","This tests that ranged add of two stereo audio files works correctly.")
 {
